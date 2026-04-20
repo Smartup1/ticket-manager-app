2729da7aa0ec44031d4255fb164615790786187b
Henrique Gomes <54337635+Smartup1@users.noreply.github.com>
2026-04-19 23:41:22 -0300

    Rename Index to Dashboard and enhance UI
    Refactor component name and update UI elements.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+export default function Dashboard() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>SmartDesk</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 22, marginBottom: 20 }}>
+        Sistema de Chamados
+      </Text>
+
+      <TouchableOpacity onPress={() => router.push("/tickets")}>
+        <Text>📋 Ver Chamados</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => router.push("/tickets/create")}>
+        <Text>➕ Novo Chamado</Text>
+      </TouchableOpacity>
     </View>
   );
 }
