3575fd659a629af00c29b39a2eac342a0aa594f5
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2026-05-10 00:07:45 -0300

    feat: implements greetings on login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,36 @@
+import { useAuthContext } from "@/contexts/auth-context";
+import { User } from "@/contexts/auth-context-types";
 import { Text, View } from "react-native";
 
+const Greetings = (user: User) => {
+  return (
+    <View>
+      <Text>Bem vindo, {user.name}!</Text>
+      <Text>Seu email é {user.email}</Text>
+      <Text>Você é um {user.role}!</Text>
+    </View>
+  )
+}
+
 export default function Index() {
+  const { user } = useAuthContext();
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
+        marginTop: 100
       }}
     >
-      <Text>SmartDesk</Text>
+      <Text style={{
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20
+      }}>
+        Ticket Manager App
+      </Text>
+      <Greetings {...user!} />
     </View>
   );
 }
